b1105b6b3f4a5b6633c88a17beee0d809d2234ef
Anna Szwal <aniaszwal@gmail.com>
2021-03-19 21:30:59 +0100

    Change function and simplify init actions, add unlove function

--- src/js/script.js
+++ src/js/script.js
@@ -12,6 +12,14 @@
     containerOf: {
       booksList: '.books-list',
     },
+
+    class: {
+      favoriteBooks: 'favorite',
+    },
+
+    book: {
+      bookImage: '',
+    }
   };
 
   const templates = {
@@ -20,37 +28,44 @@
 
   //  Dodaj nową funkcję render
 
-  function render(){
-    for (let books of dataSource.books){
+  function render() {
+    for (let books of dataSource.books) {
 
       // wygenerowanie kodu HTML na podstawie szablonu
       const generatedHTML = templates.booksTemplate(books);
       // wygeneruj element DOM z kodu HTML
       const element = utils.createDOMFromHTML(generatedHTML);
       //find books container
-      const booksContainer = document.querySelector(select.containerOf.booksList);
+      const booksListContainer = document.querySelector(select.containerOf.booksList);
       //add element to list - Wygenerowany element DOM dołącz jako nowe dziecko DOM do listy .books-list.
-      booksContainer.appendChild(element);
+      booksListContainer.appendChild(element);
     }
   }
+
   render();
 
   const favoriteBooks = [];
 
-  function initActions(){
+  const initActions = function(){
 
-    const booksContainer = document.querySelector(select.containerOf.booksList);
-    const booksImage = booksContainer.querySelectorAll('.book__image');
+    const booksListContainer = document.querySelector(select.containerOf.booksList); //referencja do listy książek
 
-    for(let image of booksImage){
-      image.addEventListener('dblclick', function(event){
-        event.preventDefault();
-        image.classList.add('favorite');
-        const id = image.getAttribute('data-id');
-        favoriteBooks.push(id);
-      });
-    }
-  }
+    booksListContainer.addEventListener('dblclick', function(event){ //nasłuchiwacz
+      event.preventDefault(); // zatrzymanie domyślnych
+      const clickedElement = event.target.offsetParent; //najbliższy kliknięty
+      const id = clickedElement.getAttribute('data-id'); // pobieranie atrybutu
+
+      // Jeśli nie jest, to funkcja ma dodać klasę favorite i zapisać id książki w tablicy favoriteBooks
+      if(!clickedElement.classList.contains('favorite')){
+        favoriteBooks.push(id); //dodanie identyfikatora do favbooks
+        clickedElement.classList.add('favorite');
+
+        //Jeśli jest, to usuń id takiej książki z tablicy favoriteBooks i usun takiemu elementowi klasę favorite.
+      }else{
+        favoriteBooks.splice(favoriteBooks.indexOf(id), 1); //usunięcie id z tablicy
+        clickedElement.classList.remove('favorite');
+      }
+    });
+  };
   initActions();
 }
-
